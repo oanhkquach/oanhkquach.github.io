4434822e625da1a455d6b85817077ffb13414307
Eric Nguyen <19nguyene@tesdk12.net>
2018-08-11 17:37:42 -0400

    allow one list to be open at a time

---
+++ js/onelist.js
@@ -0,0 +1,13 @@
+let lists = document.getElementsByClassName("list");
+
+function closeOtherLists(index) {
+  for (let i = 0; i < lists.length; i++) {
+    if (i !== index) {
+      lists[i].removeAttribute("open");
+    }
+  }
+}
+
+for (let i = 0; i < lists.length; i++) {
+  lists[i].setAttribute("onclick", "closeOtherLists(" + i + ")");
+}
