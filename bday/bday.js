17b15d7f1fa649e797fbce85781aedafb7812fd5
Eric Nguyen <19nguyene@tesdk12.net>
2018-08-15 15:11:43 -0400

    fix everything

--- bday/bday.js
+++ bday/bday.js
@@ -1,50 +1,21 @@
-function newPopUp(message) {
-  let popUp = document.createElement("div");
-  popUp.appendChild(message);
-  popUp.id = "happy-bday";
-  popUp.setAttribute("onclick", "popup.style.display = 'none'; document.body.removeChild(confettiHolder);");
-  return popUp;
+function toggle() {
+  overlay.setAttribute("onclick", "popup.style.display = 'none'; confettiHolder.style.display = 'none'");
 }
 
-function newMessage() {
-  let msg = document.createElement("h1");
-  let hostname = location.hostname;
-  let name = hostname.substr(0, hostname.indexOf("."));
-  msg.innerHTML = "Happy birthday to " + name + "!";
-  return msg;
-}
-
-function addFonts(url) {
-  let fonts = document.createElement("link");
-  fonts.href = url;
-  fonts.rel = "stylesheet";
-  document.head.appendChild(fonts);
-  return fonts;
-}
-
-function stylePopUp(popup) {
-  popup.style.width = "100%";
-  popup.style.height = "100vh";
-  popup.style.color = "white";
-  popup.style.fontFamily = "'Asap', sans-serif";
-  popup.style.textAlign = "center";
-  popup.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  popup.style.position = "fixed";
-  popup.style.display = "inline";
-  return popup;
-}
-
+// Confetti
 let confettiHolder = document.createElement("canvas");
 confettiHolder.id = "confetti-holder";
-confettiHolder.style.position = "fixed";
 confettiHolder.width = document.body.clientWidth;
 confettiHolder.height = document.body.clientHeight;
 document.body.insertBefore(confettiHolder, document.body.firstChild);
-
 let confetti = new ConfettiGenerator();
 confetti.render();
 
-let popup = newPopUp(newMessage());
-document.body.insertBefore(popup, document.body.firstChild);
-addFonts("https://fonts.googleapis.com/css?family=Asap|Old+Standard+TT");
-stylePopUp(popup);
+// Overlay
+let overlay = document.createElement("div");
+overlay.id = "happy-bday";
+let message = document.createElement("h1");
+let name = location.hostname.substr(0, location.hostname.indexOf("."));
+message.innerHTML = "Happy birthday to " + name + "!";
+overlay.appendChild(message);
+document.body.insertBefore(overlay, document.body.firstChild);
