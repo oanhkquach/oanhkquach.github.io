95a240e44b8ed79c8566abe9f9b50903e6f52a5e
Eric Nguyen <19nguyene@tesdk12.net>
2018-08-16 01:02:02 -0400

    birthday time

--- bday/bday.js
+++ bday/bday.js
@@ -1,6 +1,13 @@
+/**
+ * Copyright (c) 2018 Eric Nguyen
+ * License: MIT 
+ **/
+
 function hide() {
   confettiHolder.setAttribute("style", "display: none");
   overlay.setAttribute("style", "display: none");
+  document.getElementsByTagName("header")[0].style.filter = "none";
+  document.getElementsByTagName("main")[0].style.filter = "none";
 }
 
 // Confetti
@@ -16,8 +23,8 @@ confetti.render();
 let overlay = document.createElement("div");
 overlay.id = "happy-bday";
 let message = document.createElement("h1");
-let name = location.hostname.substr(0, location.hostname.indexOf("."));
-message.innerHTML = "Happy birthday to " + name + "!";
+// let name = location.hostname.substr(0, location.hostname.indexOf(".")); // Use URL hostname
+message.innerHTML = "Happy 18<sup>th</sup> birthday to Oanh!";
 overlay.appendChild(message);
 document.body.insertBefore(overlay, document.body.firstChild);
 
